e1221177ac4c28c2da855d0b083a3341aeb80d29
maromoz <maromohevzion@gmail.com>
2016-10-05 11:21:17 +0300

    Converting the  functions to ORM functions

--- cities/views.py
+++ cities/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.db import connection
 from django.http import HttpResponse
-from cities.models import Cities
+from cities.models import Cities, Favorite
 
 
 def get_cities(request):
@@ -27,49 +27,39 @@ def get_city_temp(request):
     return HttpResponse(temp_response)
 
 
-def favorite(request):
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT name FROM favorite')
-        row = cursor.fetchall()
-        favorite_list = list()
-        for city in row:
-            cursor.execute('SELECT name,temprature,last_updated from cities WHERE name = "%s"' % city)
-            city_forecast = cursor.fetchone()
-            favorite_list.append(city_forecast)
-    return HttpResponse(favorite_list)
+def get_favorite(request):
+    favorite_list = Favorite.objects.all()
+    response = ""
+    for item in favorite_list:
+        favorite_name = item.name
+        response += "%s<br>" % favorite_name
+    return HttpResponse(response)
 
 
-def add_to_favorite(request):
-    if request.GET.get('add', ''):
-        city_name = request.GET.get('add')
-        with connection.cursor() as cursor:
-            cursor.execute('SELECT count(*) FROM cities WHERE name = "%s"' % city_name)
-            test = cursor.fetchone()[0]
-            if test == 0:
-                not_exists = "The city does not exists please choose a different city from the cities list"
-                return HttpResponse(not_exists)
-            cursor.execute('SELECT count(*) FROM favorite WHERE name = "%s"' % city_name)
-            test = cursor.fetchone()[0]
-            if test == 1:
-                already_in_table = "The city already exists in the favorite list"
-                return HttpResponse(already_in_table)
-            cursor.execute('INSERT INTO favorite (favorite.name) VALUES ("%s")' % city_name)
-            success = "The city was added to your favorite list (:"
-            return HttpResponse(success)
+def add_city_to_favorite(request):
+    if 'add' not in request.GET:
+        return HttpResponse("please write the word add before the name of the city")
+    name = request.GET.get('add')
+    city_list = Cities.objects.filter(name=name)
+    if len(city_list) == 0:
+        return HttpResponse("The city does not exists please choose a different city from the cities list")
+    favorite_list = Favorite.objects.filter(name=name)
+    if len(favorite_list) >= 1:
+        return HttpResponse("The city already exists in the favorite list")
+    f = Favorite(name=name)
+    f.save()
+    return HttpResponse("The city was added to your favorite list (:")
 
 
-def remove_from_favorite(request):
-    if request.GET.get('remove', ''):
-        city_name = request.GET.get('remove')
-        with connection.cursor() as cursor:
-            cursor.execute('SELECT count(*) FROM favorite WHERE name = "%s"' % city_name)
-            test = cursor.fetchone()[0]
-            if test == 0:
-                not_exists = "The city does not exists please choose a different city from the favorite list"
-                return HttpResponse(not_exists)
-            cursor.execute('DELETE FROM favorite WHERE name = "%s"' % city_name)
-            success = "The city was removed from your favorite list (:"
-        return HttpResponse(success)
+def remove_city_from_favorite(request):
+    if 'remove' not in request.GET:
+        return HttpResponse("please write the word remove before the name of the city")
+    name = request.GET.get('remove')
+    favorite_list = Favorite.objects.filter(name=name)
+    if len(favorite_list) == 0:
+        return HttpResponse("The city does not exists in the favorite list")
+    favorite_list.delete()
+    return HttpResponse("The city was removed from your favorite list (:")
 
 
 #############################################OLD############################################
